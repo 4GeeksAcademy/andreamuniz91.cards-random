b945f13afcbb82ecde7aad6590296559316b59a9
Andrea Muñiz Salvadores <163444471+andreamuniz91@users.noreply.github.com>
2024-06-07 11:46:13 +0000

    terminado

--- src/app.js
+++ src/app.js
@@ -7,5 +7,21 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  newCard();
 };
+function newCard() {
+  let suits = ["♦", "♥", "♠", "♣"];
+  let numbers = ["A","1","2","3","4","5","6","7","8","9","10","J","Q","K",];    
+  
+  let indexSuits = Math.floor(Math.random() * suits.length);
+  let indexNumbers = Math.floor(Math.random() * numbers.length);
+
+  document.querySelector("#top").innerHTML = suits[indexSuits];
+  document.querySelector("#number").innerHTML = numbers[indexNumbers];
+  document.querySelector("#end").innerHTML = suits[indexSuits];
+
+  let colorSuit = (suits[indexSuits] == "♦" || suits[indexSuits] == "♥") ? "text-danger" : "text-dark";
+  document.querySelector("#top").className = colorSuit;
+  document.querySelector("#number").className = colorSuit;
+  document.querySelector("#end").className = colorSuit;
+}
